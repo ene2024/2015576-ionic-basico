d9e2fd89b3e97862f004b5c18645b2a01514ba5c
Barbara <rossana.pesil@gmail.com>
2024-02-29 08:18:32 -0600

    Practica 3

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-var artinp = document.getElementById("art");
-var preinp = document.getElementById("precio");
-
-var total = 0;
-
-const lugarticket = document.getElementById("ticket");
-
-function agregar(){
-    pesos = parseInt(preinp.value);
-    total = total + pesos;
-
-    let productos = document.createElement('ol');
-    productos.innerHTML = ' ' + artinp.value + '_ _ _ _ _ $' + pesos;
-    lugarticket.appendChild(productos);
-
-    artinp.value='';
-    preinp.value='';
-
-    let final = document.createElement('p');
-    final.innerHTML = '   Total:  ' + parseInt(total);
-    lugarticket.appendChild(final);
+function Login(usuario, contra){
+    var usr = document.getElementById("usuario");
+    var cont = document.getElementById("contra")
+    alert('Login correcto...');
 }
 
-/* no pude hacer funcionar esta funcion:/
-function dinero(total) {
-    let final = document.createElement('p');
-    final.innerHTML = '   Total:  ' + parseInt(total);
-    lugarticket.appendChild(final);
-}*/
+function Registro(usuario){
+    var usr = document.getElementById("usuario");
+    user = usr.value;
+    alert(user + ' ha sido registrado ^^');
+}
