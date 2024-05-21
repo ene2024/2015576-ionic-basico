24c96fc4481344a0e8d6129d5850fd2ed7850334
Barbara <rossana.pesil@gmail.com>
2024-05-21 03:45:49 -0600

    Practica 13: Build de App

--- miApp-2015576/capacitor.config.ts
+++ miApp-2015576/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'miApp-2015576',
   webDir: 'www',
   server: {
